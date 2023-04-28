2ceee9333b78f6b26bbfd3f6f2ea567f6a22c35b
zack965 <67732045+zack965@users.noreply.github.com>
2023-04-28 11:03:14 +0100

    globalize redis

--- app/Http/Controllers/JoinUsController.php
+++ app/Http/Controllers/JoinUsController.php
@@ -4,13 +4,26 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Category;
+use Illuminate\Support\Facades\Redis;
+
 
 
 class JoinUsController extends Controller
 {
     public function index(){
-        $title = "Nous rejoindre - BTI - Advisory";
+        /* $title = "Nous rejoindre - BTI - Advisory";
         $categories = Category::where("url_presentation",$title)->first();
-        return view('join-us')->with('title',$title)->with('articles',$categories->articles);
+        return view('join-us')->with('title',$title)->with('articles',$categories->articles); */
+        $redis_data = json_decode( Redis::get("Nous_rejoindre_BTI_Advisory"));
+        $title = "Nous rejoindre - BTI - Advisory";
+        if($redis_data){
+            //return "hhhh";
+
+            return view('join-us')->with('title', $title)->with("articles",$redis_data);
+        }else{
+            $categories = Category::where("url_presentation",$title)->first();
+            Redis::set("Nous_rejoindre_BTI_Advisory", json_encode($categories->articles));
+            return view('join-us')->with('title', $title)->with("articles",$categories->articles);
+        }
     }
 }
--- app/Http/Controllers/ServiceController.php
+++ app/Http/Controllers/ServiceController.php
@@ -19,49 +19,82 @@ class ServiceController extends Controller
 
             return view('services.all-service')->with('title', $title)->with("articles",$redis_data);
         }else{
-            return "hhhh";
 
             $categories = Category::where("url_presentation",$title)->first();
             Redis::set("Nos_service_BTI_Advisory", json_encode($categories->articles));
             return view('services.all-service')->with('title', $title)->with("articles",$categories->articles);
         }
-        //return $redis_data;
-       // return $redis_data;
-        //return json_encode($redis_data);
-        //$data = json_decode($redis_data, true);
-       //return $data;
-        /* $data = $categories->articles;
-        $json_data =  json_encode($data);
-        return $data;
-        Redis::set("Nos_service_BTI_Advisory", $json_data); */
     }
 
     public function show()
     {
 
-        $title = "Technologie & Innovation - BTI - Advisory";
+        /* $title = "Technologie & Innovation - BTI - Advisory";
         $categories = Category::where("url_presentation",$title)->first();
-        return view('services.technologie-innovation')->with('title', $title)->with("articles",$categories->articles);
+        return view('services.technologie-innovation')->with('title', $title)->with("articles",$categories->articles); */
+        $redis_data = json_decode( Redis::get("Technologie_Innovation_BTI_Advisory"));
+        $title = "Technologie & Innovation - BTI - Advisory";
+        if($redis_data){
+            //return "hhhh";
+
+            return view('services.technologie-innovation')->with('title', $title)->with("articles",$redis_data);
+        }else{
+            $categories = Category::where("url_presentation",$title)->first();
+            Redis::set("Technologie_Innovation_BTI_Advisory", json_encode($categories->articles));
+            return view('services.technologie-innovation')->with('title', $title)->with("articles",$categories->articles);
+        }
     }
 
     public function multi_speed_it()
     {
-        $title = "Multi-Speed IT - BTI - Advisory";
+        /* $title = "Multi-Speed IT - BTI - Advisory";
         $categories = Category::where("url_presentation",$title)->first();
-        return view('services.multi-speed-it')->with('title', $title)->with("articles",$categories->articles);
+        return view('services.multi-speed-it')->with('title', $title)->with("articles",$categories->articles); */
+        $redis_data = json_decode( Redis::get("Multi_Speed_IT_BTI_Advisory"));
+        $title = "Multi-Speed IT - BTI - Advisory";
+        if($redis_data){
+            //return "hhhh";
+
+            return view('services.multi-speed-it')->with('title', $title)->with("articles",$redis_data);
+        }else{
+            $categories = Category::where("url_presentation",$title)->first();
+            Redis::set("Multi_Speed_IT_BTI_Advisory", json_encode($categories->articles));
+            return view('services.multi-speed-it')->with('title', $title)->with("articles",$categories->articles);
+        }
     }
 
     public function transformation_produits_it()
     {
-        $title = "Transformation des Produits IT - BTI - Advisory";
+        /* $title = "Transformation des Produits IT - BTI - Advisory";
         $categories = Category::where("url_presentation",$title)->first();
-        return view('services.transformation-des-produits-it')->with('title', $title)->with("articles",$categories->articles);
+        return view('services.transformation-des-produits-it')->with('title', $title)->with("articles",$categories->articles); */
+        $redis_data = json_decode( Redis::get("Transformation_des_Produits_IT_BTI_Advisory"));
+        $title = "Transformation des Produits IT - BTI - Advisory";
+        if($redis_data){
+
+            return view('services.transformation-des-produits-it')->with('title', $title)->with("articles",$redis_data);
+        }else{
+            $categories = Category::where("url_presentation",$title)->first();
+            Redis::set("Transformation_des_Produits_IT_BTI_Advisory", json_encode($categories->articles));
+            return view('services.transformation-des-produits-it')->with('title', $title)->with("articles",$categories->articles);
+        }
     }
 
     public function transformation_ux()
     {
-        $title = "Transformation UX - BTI - Advisory";
+        /* $title = "Transformation UX - BTI - Advisory";
         $categories = Category::where("url_presentation",$title)->first();
-        return view('services.transformation-ux')->with('title', $title)->with("articles",$categories->articles);
+        return view('services.transformation-ux')->with('title', $title)->with("articles",$categories->articles); */
+        $redis_data = json_decode( Redis::get("Transformation_UX_BTI_Advisory"));
+        $title = "Transformation UX - BTI - Advisory";
+        if($redis_data){
+            //return "hhhh";
+
+            return view('services.transformation-ux')->with('title', $title)->with("articles",$redis_data);
+        }else{
+            $categories = Category::where("url_presentation",$title)->first();
+            Redis::set("Transformation_UX_BTI_Advisory", json_encode($categories->articles));
+            return view('services.transformation-ux')->with('title', $title)->with("articles",$categories->articles);
+        }
     }
 }
