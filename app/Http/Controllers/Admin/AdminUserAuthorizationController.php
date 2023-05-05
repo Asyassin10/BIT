ee424e32f87049aa68d4c075b6ea701487e0b30f
zack965 <67732045+zack965@users.noreply.github.com>
2023-05-05 11:05:35 +0100

    adding roles

--- app/Http/Controllers/Admin/AdminUserAuthorizationController.php
+++ app/Http/Controllers/Admin/AdminUserAuthorizationController.php
@@ -5,12 +5,24 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Spatie\Permission\Models\Role;
 
 class AdminUserAuthorizationController extends Controller
 {
     //
     public function getAllUser(){
         $users = User::all();
-        return view("admin.pages.users.getAllUser")->with("users",$users);
+        $roles_app = Role::all();
+        foreach($users as $user){
+            //$user->assignRole('User');
+            $user->roles;
+            /* foreach ($roles as $role) {
+                echo $role->name;
+            } */
+        }
+        //return $users;
+        return view("admin.pages.users.getAllUser")
+            ->with("roles_app",$roles_app)
+            ->with("users",$users);
     }
 }
