06c1e7f9e9027a4234d56c1252b012f23da4b8c7
unknown <asyassin06@gmail.com>
2023-05-17 16:08:14 +0100

    sd

--- resources/views/admin/pages/Category/allCategories.blade.php
+++ resources/views/admin/pages/Category/allCategories.blade.php
@@ -61,9 +61,9 @@
  --}}
 
 @extends('layouts/contentLayoutMaster')
+@section('title', 'Les Catégorie')
 
-@section('title', 'DataTables')
-
+<br>
 @section('vendor-style')
     {{-- vendor css files --}}
     <link rel="stylesheet" href="{{ asset(mix('vendors/css/tables/datatable/responsive.bootstrap5.min.css')) }}">
@@ -75,7 +75,6 @@
 @section('content')
     <div class="row">
         <div class="col-12">
-            <p>Read full documnetation <a href="https://datatables.net/" target="_blank">here</a></p>
         </div>
     </div>
     <!-- Basic table -->
@@ -86,136 +85,73 @@
                     <table class=" table">
                         <thead>
                             <tr>
-                                <th></th>
-                                <th></th>
-                                <th>id</th>
-                                <th>Name</th>
-                                <th>Email</th>
-                                <th>Date</th>
+                                <th>#</th>
+                                <th>Nom de Catégorie</th>
+                                <th>Référence de catégorie</th>
+                                <th>Opérations</th>
                             </tr>
                         </thead>
                         <tbody>
-                            <tr class="odd">
-                                <td>
-                                    <div class="d-flex justify-content-left align-items-center">
-                                        <div class="avatar  bg-light-dark  me-1"><span class="avatar-content">RH</span>
-                                        </div>
-                                        <div class="d-flex flex-column"><span class="emp_name text-truncate fw-bold">Ronica
-                                                Hasted</span><small class="emp_post text-truncate text-muted">Software
-                                                Consultant</small></div>
-                                    </div>
-                                </td>
-                                <td>rhasted2l@hexun.com</td>
-                                <td>07/04/2019</td>
-                                <td style="">$24866.66</td>
-                                <td style=""><span class="badge rounded-pill  badge-light-warning">Resigned</span>
-                                </td>
-                                <td style="">
-                                    <div class="d-inline-flex"><a class="pe-1 dropdown-toggle hide-arrow text-primary"
-                                            data-bs-toggle="dropdown"><svg xmlns="http://www.w3.org/2000/svg" width="24"
-                                                height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
-                                                stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                                                class="feather feather-more-vertical font-small-4">
-                                                <circle cx="12" cy="12" r="1"></circle>
-                                                <circle cx="12" cy="5" r="1"></circle>
-                                                <circle cx="12" cy="19" r="1"></circle>
-                                            </svg></a>
-                                        <div class="dropdown-menu dropdown-menu-end"><a href="javascript:;"
-                                                class="dropdown-item"><svg xmlns="http://www.w3.org/2000/svg" width="24"
-                                                    height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
-                                                    stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                                                    class="feather feather-file-text font-small-4 me-50">
-                                                    <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z">
-                                                    </path>
-                                                    <polyline points="14 2 14 8 20 8"></polyline>
-                                                    <line x1="16" y1="13" x2="8" y2="13">
-                                                    </line>
-                                                    <line x1="16" y1="17" x2="8" y2="17">
-                                                    </line>
-                                                    <polyline points="10 9 9 9 8 9"></polyline>
-                                                </svg>Details</a><a href="javascript:;" class="dropdown-item"><svg
-                                                    xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                            @foreach ($all_categories as $key => $c)
+                                <tr class="odd">
+                                    <td>{{ $key }}</td>
+                                    <td>{{ $c->categorie_name }}</td>
+                                    <td>
+                                        @if (is_null($c->categorie_parent_id))
+                                            <span class="badge rounded-pill  badge-light-danger">Nulle</span>
+                                        @else
+                                            <span
+                                                class="badge rounded-pill  badge-light-success">{{ $c->referenceCategory->categorie_name }}</span>
+                                        @endif
+                                    </td>
+                                    <td style="">
+                                        <div class="d-inline-flex"><a class="pe-1 dropdown-toggle hide-arrow text-primary"
+                                                data-bs-toggle="dropdown" href="">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
                                                     viewBox="0 0 24 24" fill="none" stroke="currentColor"
                                                     stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                                                    class="feather feather-archive font-small-4 me-50">
-                                                    <polyline points="21 8 21 21 3 21 3 8"></polyline>
-                                                    <rect x="1" y="3" width="22" height="5">
-                                                    </rect>
-                                                    <line x1="10" y1="12" x2="14" y2="12">
-                                                    </line>
-                                                </svg>Archive</a><a href="javascript:;"
-                                                class="dropdown-item delete-record"><svg xmlns="http://www.w3.org/2000/svg"
-                                                    width="24" height="24" viewBox="0 0 24 24" fill="none"
-                                                    stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                                    stroke-linejoin="round"
-                                                    class="feather feather-trash-2 font-small-4 me-50">
-                                                    <polyline points="3 6 5 6 21 6"></polyline>
-                                                    <path
-                                                        d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2">
-                                                    </path>
-                                                    <line x1="10" y1="11" x2="10" y2="17">
-                                                    </line>
-                                                    <line x1="14" y1="11" x2="14" y2="17">
-                                                    </line>
-                                                </svg>Delete</a></div>
-                                    </div><a href="javascript:;" class="item-edit"><svg
-                                            xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                            viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                                            stroke-linecap="round" stroke-linejoin="round"
-                                            class="feather feather-edit font-small-4">
-                                            <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path>
-                                            <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path>
-                                        </svg></a>
-                                </td>
-                            </tr>
-
+                                                    class="feather feather-more-vertical font-small-4">
+                                                    <circle cx="12" cy="12" r="1"></circle>
+                                                    <circle cx="12" cy="5" r="1"></circle>
+                                                    <circle cx="12" cy="19" r="1"></circle>
+                                                </svg></a>
+                                            <div class="dropdown-menu dropdown-menu-end">
+                                                <a href="{{ route('AllArticles', ['category_id' => $c->categorie_id]) }}"
+                                                    class="dropdown-item"><svg xmlns="http://www.w3.org/2000/svg"
+                                                        width="24" height="24" viewBox="0 0 24 24" fill="none"
+                                                        stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                                        stroke-linejoin="round"
+                                                        class="feather feather-file-text font-small-4 me-50">
+                                                        <path
+                                                            d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z">
+                                                        </path>
+                                                        <polyline points="14 2 14 8 20 8"></polyline>
+                                                        <line x1="16" y1="13" x2="8" y2="13">
+                                                        </line>
+                                                        <line x1="16" y1="17" x2="8" y2="17">
+                                                        </line>
+                                                        <polyline points="10 9 9 9 8 9"></polyline>
+                                                    </svg>Articles</a>
+                                            </div>
+                                        </div><a href="{{ route('UpdateCategory', ['category_id' => $c->categorie_id]) }}"
+                                            class="item-edit"><svg xmlns="http://www.w3.org/2000/svg" width="24"
+                                                height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                                stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                                                class="feather feather-edit font-small-4">
+                                                <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7">
+                                                </path>
+                                                <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path>
+                                            </svg></a>
+                                    </td>
+                                </tr>
+                            @endforeach
                         </tbody>
 
                     </table>
                 </div>
             </div>
         </div>
-        <!-- Modal to add new record -->
-        <div class="modal modal-slide-in fade" id="modals-slide-in">
-            <div class="modal-dialog sidebar-sm">
-                <form class="add-new-record modal-content pt-0">
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">×</button>
-                    <div class="modal-header mb-1">
-                        <h5 class="modal-title" id="exampleModalLabel">New Record</h5>
-                    </div>
-                    <div class="modal-body flex-grow-1">
-                        <div class="mb-1">
-                            <label class="form-label" for="basic-icon-default-fullname">Full Name</label>
-                            <input type="text" class="form-control dt-full-name" id="basic-icon-default-fullname"
-                                placeholder="John Doe" aria-label="John Doe" />
-                        </div>
-                        <div class="mb-1">
-                            <label class="form-label" for="basic-icon-default-post">Post</label>
-                            <input type="text" id="basic-icon-default-post" class="form-control dt-post"
-                                placeholder="Web Developer" aria-label="Web Developer" />
-                        </div>
-                        <div class="mb-1">
-                            <label class="form-label" for="basic-icon-default-email">Email</label>
-                            <input type="text" id="basic-icon-default-email" class="form-control dt-email"
-                                placeholder="john.doe@example.com" aria-label="john.doe@example.com" />
-                            <small class="form-text"> You can use letters, numbers & periods </small>
-                        </div>
-                        <div class="mb-1">
-                            <label class="form-label" for="basic-icon-default-date">Joining Date</label>
-                            <input type="text" class="form-control dt-date" id="basic-icon-default-date"
-                                placeholder="MM/DD/YYYY" aria-label="MM/DD/YYYY" />
-                        </div>
-                        <div class="mb-4">
-                            <label class="form-label" for="basic-icon-default-salary">Salary</label>
-                            <input type="text" id="basic-icon-default-salary" class="form-control dt-salary"
-                                placeholder="$12000" aria-label="$12000" />
-                        </div>
-                        <button type="button" class="btn btn-primary data-submit me-1">Submit</button>
-                        <button type="reset" class="btn btn-outline-secondary" data-bs-dismiss="modal">Cancel</button>
-                    </div>
-                </form>
-            </div>
-        </div>
+
     </section>
     <!--/ Basic table -->
 
