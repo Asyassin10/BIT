55f516832e2f5c76b3e65eff0e43c190fd0dc58f
zack965 <67732045+zack965@users.noreply.github.com>
2023-05-31 13:32:33 +0100

    addine new page

--- routes/web.php
+++ routes/web.php
@@ -57,6 +57,7 @@ Route::get('/nos-services', [ServiceController::class, 'index'])->name('services
 
 Route::prefix('service')->group(function () {
     Route::get('/construire-la-vision', [ServiceController::class, 'Construire_la_vision'])->name('Construire_la_vision');
+    Route::get('/expérience-utilisateur', [ServiceController::class, 'expérience_utilisateur'])->name('expérience_utilisateur');
     Route::get('/accompagner-la-mise-en-oeuvre', [ServiceController::class, 'Accompagner_la_mise_en_oeuvre'])->name('Accompagner_la_mise_en_oeuvre');
 
     Route::get('/technologie-innovation', [ServiceController::class, 'show'])->name('technologie-innovation.service');
