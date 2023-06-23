da8e77e63ba18383b18b2fa8b68a6cfa56e07cf3
zack965 <67732045+zack965@users.noreply.github.com>
2023-06-23 11:35:10 +0100

    halaloya

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -164,7 +164,7 @@
                                                                     <img class="hfe-site-logo-img elementor-animation- lazy"
                                                                         src="{{ secure_asset('images/navbar/logo.svg') }}"
                                                                         data-src="{{ secure_asset('images/navbar/logo.svg') }}"
-                                                                        alt="">
+                                                                        id="image_lolo_webp" alt="">
                                                                 </picture>
                                                             </div>
                                                         </div>
@@ -174,6 +174,32 @@
                                         </div>
                                     </div>
                                 </div>
+                                <script>
+                                    window.addEventListener('scroll', function() {
+                                        var image = document.getElementById('image_lolo_webp');
+                                        var scrollPosition = window.scrollY;
+                                        var pageHeight = document.documentElement.scrollHeight - window.innerHeight;
+                                        var newSize = 80; // Set the desired new size for the image
+
+                                        if (scrollPosition <= 0) {
+                                            // Scroll position is at the top or above the top
+                                            newSize = 150; // Set the original size for the image
+                                        } else if (scrollPosition >= pageHeight * 1.5) {
+                                            // Scroll position has reached 50% of the page heighta
+                                            //alert("hhh")
+                                            newSize = 80; // Set the desired smaller size for the image
+                                        } else {
+                                            // Scroll position is between top and 50% of the page height
+                                            /* var shrinkFactor = 0.5; // Adjust the shrinking rate as desired
+                                            newSize = 400 - (scrollPosition / (pageHeight * 0.5)) * (400 - 200); */
+                                            //newSize = Math.max(newSize, 200); // Ensure newSize doesn't go below 200
+                                        }
+                                        image.style.transition = 'width 0.3s, height 0.3s';
+                                        // Set the new size for the image
+                                        image.style.width = newSize + 'px';
+                                        image.style.height = newSize * 1 + 'px'; // Maintain the aspect ratio if necessary
+                                    });
+                                </script>
                                 <nav class="has_ae_slider elementor-column elementor-col-33 elementor-top-column elementor-element elementor-element-79f36302 ae-bg-gallery-type-default"
                                     data-id="79f36302" data-element_type="column">
                                     <div class="elementor-widget-wrap elementor-element-populated">
@@ -665,7 +691,7 @@
                                                         /*! elementor - v3.8.1 - 13-11-2022 */
                                                         /* .elementor-element{
 
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    } */
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                } */
                                                         .elementor-widget-image {
                                                             text-align: center
                                                         }
